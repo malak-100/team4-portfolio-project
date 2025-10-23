ed86d38af58ebd261b8b68c2c0d40079dffb2f4c
Malak_99 <malak.felioune@ensia.edu.dz>
2025-10-23 17:37:04 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,29 @@ import { members } from "./interfaces/member";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      {/* 
+        This is the key change. 
+        The `basename` prop tells React Router that your app is served from a subdirectory.
+        In local development, `import.meta.env.BASE_URL` will be '/',
+        and in the build for GitHub pages, it will be '/team4-portfolio-project/'.
+      */}
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <div className="bg-background-light dark:bg-background-dark font-display text-gray-800 dark:text-gray-200">
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
             {members.map((member) => (
               <Route
+                key={member.route} // It's a good practice to add a unique key prop here
                 path={`/${member.route}`}
                 element={<Member member={member} />}
               />
             ))}
-            //Only add an entry for you at /interfaces/member.ts //Add your
-            photos in the corresponding folder at /public
+            {/* //Only add an entry for you at /interfaces/member.ts */}
+            {/* //Add your photos in the corresponding folder at /public */}
           </Routes>
-        </div>{" "}
-      </BrowserRouter>{" "}
+        </div>
+      </BrowserRouter>
     </>
   );
 }
