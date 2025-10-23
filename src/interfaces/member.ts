355680495db1241eba0a9a9f1c6b203c004ad39d
Malak_99 <malak.felioune@ensia.edu.dz>
2025-10-23 09:57:07 +0100

    added the project

--- src/interfaces/member.ts
+++ src/interfaces/member.ts
@@ -25,37 +25,44 @@ export interface MemberData {
   projects: Project[] | null;
 }
 
-const youcef: MemberData = {
-  route: "youcef",
-  name: "Youcef",
-  lastname: "Belaib",
-  photo_url: "/pfps/Male.png",
-  role: "Team Leader and Software Engineer",
+const malak: MemberData = {
+  route: "malak",
+  name: "Malak",
+  lastname: "Felioune",
+  photo_url: "/pfps/malak.jpg",
+  role: "Software engineer",
   city: "Algiers",
   country: "Algeria",
-  bio: "I am a passionate software engineer who loves his team",
-  skills: ["React", "Javascript", "Flutter", "Machine Learning", "TypeScript"],
+  bio: "I am the CEO & Founder of Adam AGI, first robotic computer in the world, love deeptech and very high impact software",
+  skills: ["AGI", "Cyber security", "system engineering", "large language models", "Reinforcment learning", "and a lot more that I am tired to write"],
   experiences: [
     {
-      title: "Ai/Data Science student at ENSIA",
+      title: "student at ENSIA",
       start_date: "9/2023",
       end_date: "Present",
       type: "study",
     },
     {
-      title: "Co-Manager of External Relations at M4T club",
-      start_date: "12/2024",
+      title: "Cyber security: self employment",
+      start_date: "1/2024",
       end_date: "Present",
       type: "work",
     },
     {
-      title: "Co-Manager of Finance and Legal at EBEC club",
-      start_date: "9/2025",
+      title: "CEO and solo founder of Adam AGI",
+      start_date: "8/2025",
       end_date: "Present",
       type: "work",
     },
   ],
-  projects: null,
+  projects: [
+  {
+    title: "Adam AGI",
+    description:
+      "Adam AGI is a groundbreaking artificial general intelligence system — designed to reason, learn, and interact with the world in a fully autonomous way. It merges large-scale neural networks with real-time robotics control, making it one of the most ambitious AI implementations of 2026.",
+    photo_url: "/pfps/AdamWithCat.jpg"
+  }
+],
 };
 
-export const members = [youcef];
+export const members = [malak];
